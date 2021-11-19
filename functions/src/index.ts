935e0a62b6eb3d9d246ea1bb2389e5dd66e82b02
EmJee <67376694+EmJee1@users.noreply.github.com>
2021-11-19 13:38:39 +0100

    Improved logging

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,9 @@
 import * as functions from 'firebase-functions'
 import { getKDGServices } from './utils/kdg-helpers'
-import { serviceProcessingFlow } from './utils/web-file-helpers'
+import {
+	getServiceFileName,
+	serviceProcessingFlow,
+} from './utils/web-file-helpers'
 
 export const syncRecentServices = functions.pubsub
 	.schedule('59 23 * * 7')
@@ -14,6 +17,8 @@ export const syncRecentServices = functions.pubsub
 					// stop processing of remaining services, because rss feed is chronologically ordered
 					break
 				}
+
+				functions.logger.info(`${getServiceFileName(item)} added`)
 			}
 		} catch (err) {
 			functions.logger.error(err)
