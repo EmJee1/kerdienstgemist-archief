ef971fe61ad3a5234c212474ae9a646aa308f3a1
EmJee <mart-janroeleveld@outlook.com>
2021-11-21 17:33:53 +0100

    Removed unused helper files

--- web/src/util/service-helpers.ts
+++
@@ -1,7 +0,0 @@
-import { IService } from '../models/kerdienst-gemist'
-
-export const getServiceFileName = (service: IService) =>
-	service.file.split('/')[service.file.split('/').length - 1].toLowerCase()
-
-export const getServiceId = (service: IService) =>
-	getServiceFileName(service).split('-')[0]
