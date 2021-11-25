6b96c32ff7ba2a1ab5c0f7e495f39f6f4c7bdb96
EmJee <67376694+EmJee1@users.noreply.github.com>
2021-11-25 17:54:31 +0100

    Added removal of iframes

--- web/src/components/modals/IFrameEmbeddingModal.vue
+++ web/src/components/modals/IFrameEmbeddingModal.vue
@@ -20,6 +20,17 @@
 					kiest voor verwijderen.
 				</template>
 			</Notification>
+			<Notification :type="ColorType.Danger" v-if="error">
+				<template v-slot:header>Fout bij het verwijderen</template>
+				<template v-slot:body>
+					Bij het verwijderen van het iframe is er iets fout gegaan, probeer het
+					zometeen opnieuw. Neem contact op met
+					<a href="mailto:mart-janroeleveld@outlook.com">
+						mart-janroeleveld@outlook.com
+					</a>
+					als dit blijft gebeuren
+				</template>
+			</Notification>
 			<form @submit.prevent="onDeleteSubmit">
 				<label for="iframe-name" class="label">
 					Type de naam van dit iFrame ({{ iFrame.name }}) om te verwijderen
@@ -33,6 +44,7 @@
 				>
 					Verwijderen
 				</Button>
+				<Loader v-if="loading" />
 			</form>
 		</template>
 	</Modal>
@@ -40,16 +52,21 @@
 
 <script setup lang="ts">
 import { ref, computed } from 'vue'
+import { deleteDoc, doc } from '@firebase/firestore'
 import Modal from '../Modal.vue'
 import Button from '../Button.vue'
+import Loader from '../Loader.vue'
 import InputCopy from '../InputCopy.vue'
 import Notification from '../Notification.vue'
+import { firestore } from '../../firebase/firebase'
 import { IIFrameEmbed } from '../../models/embedding'
 import { ColorType } from '../../models/styling'
 
-defineEmits<{ (e: 'close'): void }>()
+const emit = defineEmits<{ (e: 'close'): void }>()
 const props = defineProps<{ iFrame: IIFrameEmbed }>()
 
+const error = ref(false)
+const loading = ref(false)
 const iFrameName = ref('')
 
 const iFrameCode = computed(
@@ -57,7 +74,19 @@ const iFrameCode = computed(
 		`https://kerdienstgemist-archief.web.app/iframe-embed?apiKey=${props.iFrame.apiKey}`
 )
 
-const onDeleteSubmit = (e: Event) => {
+const onDeleteSubmit = async (e: Event) => {
 	if (props.iFrame.name !== iFrameName.value) return
+
+	loading.value = true
+
+	try {
+		await deleteDoc(doc(firestore, 'iframes', props.iFrame.id))
+	} catch (err) {
+		console.error(err)
+		error.value = true
+	} finally {
+		loading.value = false
+		emit('close')
+	}
 }
 </script>
