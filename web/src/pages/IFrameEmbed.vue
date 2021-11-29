65dec16efe6ab6460dd996b800366b256d741cee
EmJee <67376694+EmJee1@users.noreply.github.com>
2021-11-29 20:16:13 +0100

    Updated iFrameEmbed page

--- web/src/pages/IFrameEmbed.vue
+++ web/src/pages/IFrameEmbed.vue
@@ -1,17 +1,33 @@
 <template>
 	<h1 class="title">iFrame Embedding</h1>
+	<p v-if="authorized === AuthState.UnAuthenticated">
+		Je hebt geen rechten om deze pagina te bekijken
+	</p>
+	<p v-else>Je hebt rechten, congrats!</p>
 </template>
 
 <script setup lang="ts">
-import { onMounted } from 'vue'
+import { onMounted, ref } from 'vue'
 import { useRoute } from 'vue-router'
+import { AuthState } from '../composables/use-auth-state'
+import useFullpageLoader from '../composables/use-fullpage-loader'
 
 const route = useRoute()
+const { loaderActive } = useFullpageLoader()
+
+const authorized = ref<AuthState>(AuthState.Loading)
 
 onMounted(() => {
-	// check if has valid apiKey
-	const apiKey = route.query.apiKey
+	loaderActive.value = true
+
+	const apiKey = route.query.apiKey?.toString()
+
+	if (!apiKey) {
+		authorized.value = AuthState.UnAuthenticated
+	}
+
+	loaderActive.value = false
+
 	console.log(apiKey)
-	// fetch results
 })
 </script>
