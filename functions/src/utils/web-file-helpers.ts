e4a7a980fb43c7d24b64bee6142869b9b1448644
EmJee <mart-janroeleveld@outlook.com>
2023-06-08 07:48:47 +0200

    refactor: do not re-throw unnecessarily

--- functions/src/utils/web-file-helpers.ts
+++ functions/src/utils/web-file-helpers.ts
@@ -4,103 +4,86 @@ import { IKDGService, IService } from '../models/kerkdienst-gemist'
 import { File } from '@google-cloud/storage'
 
 export const downloadFromUrl = async (
-	url: string,
-	options?: AxiosRequestConfig<any>
+  url: string,
+  options?: AxiosRequestConfig<any>
 ): Promise<[any, string]> => {
-	try {
-		const res = await axios.get(url, { responseType: 'stream', ...options })
+  const res = await axios.get(url, {responseType: 'stream', ...options})
+  if (res.status !== 200) {
+    throw new Error(`Did not get a valid response from ${url}`)
+  }
 
-		if (res.status !== 200) {
-			throw new Error(`Did not get a valid response from ${url}`)
-		}
-
-		return [res.data, res.headers['content-type']]
-	} catch (err) {
-		throw err
-	}
+  return [res.data, res.headers['content-type']]
 }
 
 export const uploadFileToStorage = async (
-	data: any,
-	fileName: string,
-	contentType: string
+  data: any,
+  fileName: string,
+  contentType: string
 ): Promise<[string, File]> => {
-	try {
-		const fileLocation = `services/audio/${fileName}`
-		const file = bucket.file(fileLocation)
-		const writeStream = file.createWriteStream({ metadata: { contentType } })
-		await data.pipe(writeStream)
+  const fileLocation = `services/audio/${fileName}`
+  const file = bucket.file(fileLocation)
+  const writeStream = file.createWriteStream({metadata: {contentType}})
+  await data.pipe(writeStream)
 
-		return [fileLocation, file]
-	} catch (err) {
-		throw err
-	}
+  return [fileLocation, file]
 }
 
 export const getServiceFileName = (service: IKDGService) =>
-	service.link.split('/')[service.link.split('/').length - 1].toLowerCase()
+  service.link.split('/')[service.link.split('/').length - 1].toLowerCase()
 
 export const getServiceId = (service: IKDGService) =>
-	getServiceFileName(service).split('-')[0]
+  getServiceFileName(service).split('-')[0]
 
 export const insertServiceToFirestore = async (
-	service: IKDGService,
-	location: string,
-	file: File
+  service: IKDGService,
+  location: string,
+  file: File
 ) => {
-	const docData: IService = {
-		title: service.title,
-		file: location,
-		createdAt: new Date(service.pubDate),
-	}
+  const docData: IService = {
+    title: service.title,
+    file: location,
+    createdAt: new Date(service.pubDate),
+  }
 
-	try {
-		const docId = getServiceId(service)
-		await firestore.collection('services').doc(docId).set(docData)
-	} catch (err) {
-		await file.delete()
-		throw new Error(
-			`Error while inserting ${service.title} from date ${service.pubDate}`
-		)
-	}
+  try {
+    const docId = getServiceId(service)
+    await firestore.collection('services').doc(docId).set(docData)
+  } catch (err) {
+    await file.delete()
+    throw new Error(
+      `Error while inserting ${service.title} from date ${service.pubDate}`
+    )
+  }
 }
 
 export const serviceExistsInFirestore = async (service: IKDGService) => {
-	try {
-		const docId = getServiceId(service)
-		return (await firestore.collection('services').doc(docId).get()).exists
-	} catch (err) {
-		throw err
-	}
+  const docId = getServiceId(service)
+  return (await firestore.collection('services').doc(docId).get()).exists
 }
 
 export const serviceProcessingFlow = async (service: IKDGService) => {
-	const fileName = getServiceFileName(service)
+  const fileName = getServiceFileName(service)
 
-	try {
-		if (await serviceExistsInFirestore(service)) {
-			return false
-		}
+  if (await serviceExistsInFirestore(service)) {
+    return false
+  }
 
-		const [rawData, contentType] = await downloadFromUrl(service.enclosure.url)
-		const [fileLocation, file] = await uploadFileToStorage(
-			rawData,
-			fileName,
-			contentType
-		)
+  const [rawData, contentType] = await downloadFromUrl(service.enclosure.url)
+  const [fileLocation, file] = await uploadFileToStorage(
+    rawData,
+    fileName,
+    contentType
+  )
 
-		await insertServiceToFirestore(service, fileLocation, file)
+  await insertServiceToFirestore(service, fileLocation, file)
 
-		return true
-	} catch (err) {
-		throw err
-	}
+  return true
 }
 
 interface GetExpirationDateProps {
-	days?: number
-	hours?: number
-	minutes?: number
+  days?: number
+  hours?: number
+  minutes?: number
 }
 
 export const getExpirationDate = ({
@@ -108,11 +91,11 @@ export const getExpirationDate = ({
 	hours,
 	minutes,
 }: GetExpirationDateProps) => {
-	const date = new Date()
+  const date = new Date()
 
-	if (days) date.setDate(date.getDate() + days)
-	if (hours) date.setHours(date.getHours() + hours)
-	if (minutes) date.setMinutes(date.getMinutes() + minutes)
+  if (days) date.setDate(date.getDate() + days)
+  if (hours) date.setHours(date.getHours() + hours)
+  if (minutes) date.setMinutes(date.getMinutes() + minutes)
 
-	return date
+  return date
 }
